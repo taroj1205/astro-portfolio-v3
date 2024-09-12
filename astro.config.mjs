885797a34f53dc9260a605b4e4563d0a318823c8
taroj1205 <taroj1205@gmail.com>
2024-09-12 22:21:34 +1200

    refactor(config): update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import playformCompress from "@playform/compress"
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://taroj.netlify.app/',
   image: {
     service: {
        entrypoint: 'astro/assets/services/sharp',
