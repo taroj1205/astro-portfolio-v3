cc0f4ff4d304479ec6a2f55d5c4c5cac9d45bf74
taroj1205 <taroj1205@gmail.com>
2024-09-19 19:44:50 +1200

    refactor(motion): updated to not use motion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,6 +53,49 @@ const config = {
       fontFamily: {
         sans: ['Inter', ...fontFamily.sans],
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '25%': { transform: 'rotate(-10deg)' },
+          '50%': { transform: 'rotate(10deg)' },
+          '75%': { transform: 'rotate(-10deg)' },
+        },
+        rotateIcon: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '25%': { transform: 'rotate(-10deg)' },
+          '50%': { transform: 'rotate(10deg)' },
+          '75%': { transform: 'rotate(-10deg)' },
+        },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+        fadeUp: {
+          '0%': { opacity: '0', transform: 'translateY(50px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeDownPull: {
+          '0%': { opacity: '0', transform: 'translateY(-50px)' },
+          '20%': { opacity: '0.5', transform: 'translateY(0)' },
+          '40%': { opacity: '1', transform: 'translateY(15px)' },
+          '60%': { opacity: '1', transform: 'translateY(-15px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle var(--animation-duration, 0.5s) ease-in-out forwards',
+        'fade-in':
+          'fadeIn var(--animation-duration, 0.3s) ease-in-out forwards',
+        'fade-out':
+          'fadeOut var(--animation-duration, 0.3s) ease-in-out forwards',
+        'fade-up': 'fadeUp var(--animation-duration, 0.5s) ease-out forwards',
+        'fade-down-pull':
+          'fadeDownPull var(--animation-duration, 1s) ease-out forwards',
+      },
     },
   },
   plugins: [require('tailwindcss-animate'), typography],
