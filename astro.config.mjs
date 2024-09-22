f39440a66da460f8aa9c735e5243eefaea8398a9
taroj1205 <taroj1205@gmail.com>
2024-09-22 20:06:54 +1200

    feat(experiences): added experiences section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
-
 import compress from 'astro-compress';
 
 // https://astro.build/config
