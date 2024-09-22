beb3e0e6599950315f7b10d18068368f50a945ad
taroj1205 <taroj1205@gmail.com>
2024-09-22 19:52:48 +1200

    feat(i18n): added some translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://taroj.netlify.app/',
+  site: 'https://taroj.pages.dev/',
   image: {
     service: {
        entrypoint: 'astro/assets/services/sharp',
@@ -14,6 +14,12 @@ export default defineConfig({
          limitInputPixels: false,
       },
     }
+  },
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ja'],
+    directory: 'src/i18n',
+    fallbackLocale: 'en',
   },
   integrations: [tailwind({
     applyBaseStyles: false,
